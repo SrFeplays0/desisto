b2dbcd255a0b5f0ec99bfd5cbf5e0544f4c41e2c
SrFeplays0 <149205297+SrFeplays0@users.noreply.github.com>
2023-11-12 23:18:44 -0300

    alguma coisa
    a

---
+++ server/index.js
@@ -0,0 +1,15 @@
+document.getElementById('customerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var address = document.getElementById('address').value;
+
+    console.log('Cadastro do Cliente:');
+    console.log('Nome:', name);
+    console.log('Email:', email);
+    console.log('Telefone:', phone);
+    console.log('Endereço:', address);
+
+});
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const express = require('express');
+const app = express();
+
+// Middleware para servir arquivos estáticos
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.post('/submit-form', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const address = req.body.address;
+
+    console.log('Nome:', name);
+    console.log('Email:', email);
+    console.log('Telefone:', phone);
+    console.log('Endereço:', address);
+
+    res.send('Formulário recebido!');
+});
